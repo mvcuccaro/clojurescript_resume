f2e5203670c2ebb1d9baa01a24f9687fbea1c451
mvcuccaro <{ID}+{username}@users.noreply.github.com>
2023-11-03 21:33:12 -0400

    remove commented code

--- src/app/views/sandbox.cljs
+++ src/app/views/sandbox.cljs
@@ -1,20 +1,8 @@
 (ns app.views.sandbox
   (:require [app.components.md5er :as md5er]))
 
-;; (defn mymd5 [s]
-;;   (let [md5 (goog.crypt.Md5.)]
-;;     (->> s (goog.crypt/stringToUtf8ByteArray)
-;;          (.update md5))
-    
-;;     (->> (.digest md5)
-;;          (goog.crypt/byteArrayToHex))))
-        
-
-;;(def hashin (r/atom "enter text here"))
-
-
 (defn main []
   [:div {:class "m-4"}
    [:div {:class "h1"} "Sandbox"]
-   [md5er/main]
-   [:hr]])
+   [:hr]
+   [md5er/main]])
